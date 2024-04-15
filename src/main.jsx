f318b24791e588200ec63bb797c3bc03a5d7d62a
Kerpo <damien.riandiere@outlook.fr>
2024-04-15 15:52:52 +0200

    Create css + main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/error-page.jsx";
+import HomePage from './components/HomePage.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+import ProjectPage from './components/ProjectPage.jsx';
+import ContactPage from './components/ContactPage.jsx';
+import Navbar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/projects",
+    element: <ProjectPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Navbar />
+    <RouterProvider router={router} />
+    <Footer />
+  </React.StrictMode>
+);
